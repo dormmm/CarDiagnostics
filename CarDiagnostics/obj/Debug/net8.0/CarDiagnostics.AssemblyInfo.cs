4d835e0c054d38c67fdc669337310edcbfb5c88c
dormmm <dormazor44321@gmail.com>
2025-02-09 13:24:46 +0200

    test2

--- CarDiagnostics/obj/Debug/net8.0/CarDiagnostics.AssemblyInfo.cs
+++ CarDiagnostics/obj/Debug/net8.0/CarDiagnostics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDiagnostics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99946e9ed18e7a34efbfed6358006df68f14fe71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c379349371732f7082d533dcb406774da41c0730")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDiagnostics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDiagnostics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
