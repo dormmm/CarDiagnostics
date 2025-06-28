c6e966e1ec19688b23a71747da456a465c833dd7
dormmm <dormazor44321@gmail.com>
2025-06-28 22:53:36 +0300

    docker

--- CarDiagnostics/obj/Release/net8.0/CarDiagnostics.AssemblyInfo.cs
+++ CarDiagnostics/obj/Release/net8.0/CarDiagnostics.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDiagnostics")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14829fcd3adb5ce248ade772bc17f4a7d4a2bd68")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDiagnostics")]
